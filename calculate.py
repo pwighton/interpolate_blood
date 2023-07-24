7b24e1d36c95c7a0fecf2fa1c4f0e5fd850ed964
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-07-24 14:39:01 -0400

    added pybids, started to calculate across a dataset

---
+++ calculate.py
@@ -0,0 +1,92 @@
+from interpolate import interpolate
+import argparse
+from bids import BIDSLayout
+import pandas
+from pathlib import Path
+import re
+
+parser = argparse.ArgumentParser(description='Interpolate blood activity curves to match frame timing of PET images')
+parser.add_argument('bidsdataset', help='Path to bids dataset')
+
+
+# given a bids dataset containing pet images, we want to create a set of tsv files containing the interpolated blood
+# activity curves matching the frame timing of the pet images. This is done by using the interpolate function from
+# interpolate.py
+
+def calculate_tacs_at_frametimes(bids_dataset_path: Path):
+    # obtain the path to the bids dataset and load with pybids
+    layout = BIDSLayout(bids_dataset_path)
+
+    # collect all time activity curves from the dataset
+    original_tacs = layout.get(suffix='blood', extension=['.tsv', '.json'])
+
+    # collect subject id's where tacs exist
+    subjects = []
+    for tac in original_tacs:
+        subjects.append(tac.entities['subject'])
+    subjects = list(set(subjects))
+    subjects.sort()
+    print(f"Found the following subjects with blood time activity curves: {subjects}")
+
+    # collect frame timing info from every subject that has a tac
+    frame_times = {}
+    for subject in subjects:
+        pet_scans = layout.get(subject=subject, suffix='pet', extension=['.nii.gz', 'nii.gz'])
+        for pet_scan in pet_scans:
+            pet_scan_path = pet_scan.path
+            metadata = pet_scan.get_metadata()
+            session = pet_scan.get_entities().get('session')
+            frame_times[subject] = frame_times.get(subject, {})
+
+            if session:
+                frame_times[subject]['Sessions'] = frame_times[subject].get('Sessions', {})
+                frame_times[subject]['Sessions'][session] = {
+                                            'Nifti': pet_scan_path,
+                                            'SessionID': session,
+                                            'FrameTimesStart': metadata['FrameTimesStart'], 
+                                            'manual': {'.json': "", '.tsv': ""}, 
+                                            'autosampler': {'.json': "", '.tsv': ""}
+                                            }
+            else:
+                frame_times[subject] = {
+                                        'Nifti': pet_scan_path,
+                                        'FrameTimesStart': metadata['FrameTimesStart'], 
+                                        'manual': {'.json': "", '.tsv': ""}, 
+                                        'autosampler': {'.json': "", '.tsv': ""}
+                                        }
+
+    for tac in original_tacs:
+        subject = tac.entities['subject']
+        session = tac.entities['session']
+        recording = tac.entities['recording']
+        extension = tac.entities['extension']
+        try:
+            if session: 
+                frame_times[subject]['Sessions'][session][recording][extension] = tac.path
+            else:
+                frame_times[subject][recording][extension] = tac.path
+        except KeyError as err:
+            print(err)
+
+    # now that we've collected all the requisite files we can interpolate the blood activity curves at each frame position
+    for subject, data in frame_times.items():
+        if 'Sessions' in data.keys():
+            for session, session_data in data['Sessions'].items():
+                manual_tsv = session_data.get('manual', {}).get('.tsv', "")
+                autosampler_tsv = session_data.get('autosampler', {}).get('.tsv', "")
+                frame_start_times = session_data.get('FrameTimesStart', [])
+                interpolated = interpolate(manual_input=manual_tsv, auto_input=autosampler_tsv, frame_time=frame_start_times)
+                frame_times[subject]['Sessions'][session]['interpolated_tac'] = interpolated
+        else:
+            for recording, recording_data in data.items():
+                for extension, path in recording_data.items():
+                    if extension == '.tsv':
+                        frame_times[subject][recording]['interpolated'] = interpolate(path, frame_times[subject]['FrameTimesStart'])
+
+    # now that we've interpolated the blood activity curves we can write them to a tsv file
+    pass
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    calculate_tacs_at_frametimes(Path(args.bidsdataset))
---
+++ interpolate.py
@@ -0,0 +1,90 @@
+import numpy
+import scipy
+import argparse
+import pandas
+import matplotlib.pyplot as plt
+from typing import Union
+
+
+
+class OutOfRangeError(Exception):
+    pass
+
+FloatList = list[float]
+
+def interpolate(manual_input, auto_input, frame_time:Union[float, FloatList], plot=False):
+
+    if isinstance(frame_time, float) or isinstance(frame_time, int):
+        frame_time = [frame_time]
+    elif isinstance(frame_time, list):
+        frame_time = [float(time) for time in frame_time]
+    else:
+        frame_time = [float(frame_time)]
+
+    # Read in the blood data
+    manual_blood_data = pandas.read_csv(manual_input, delimiter='\t')
+    auto_blood_data = pandas.read_csv(auto_input, delimiter='\t')
+
+    # combine whole blood data from manual and auto files into one dataframe
+    # pandas deprecates stuff like append so we're just going to use lists from here on out
+
+    auto_blood_time = auto_blood_data.time.tolist()
+    auto_whole_blood_radioactivity = auto_blood_data.whole_blood_radioactivity.tolist()
+
+
+    # collect the time curve into a list
+    time_curve = manual_blood_data.time.tolist()
+    whole_blood_radioactivity = manual_blood_data.whole_blood_radioactivity.tolist()
+
+    if len(auto_blood_time) == len(auto_whole_blood_radioactivity) and len(auto_blood_time) > 0:
+        time_curve.extend(auto_blood_time)
+        whole_blood_radioactivity.extend(auto_whole_blood_radioactivity)
+
+    # create a pandas dataframe and sort by time
+    blood_data = pandas.DataFrame({'time': time_curve, 'whole_blood_radioactivity': whole_blood_radioactivity})
+    blood_data.sort_values(by=['time'], inplace=True)
+
+    time_curve = blood_data.time.tolist()
+    whole_blood_radioactivity = blood_data.whole_blood_radioactivity.tolist()
+
+    # interpolate the data
+    # scipy.interpolate.Akima1DInterpolator(x, y, axis=0)
+    interpolate = scipy.interpolate.Akima1DInterpolator(time_curve, whole_blood_radioactivity, axis=0)
+
+    # return a point along the curve at the frame time
+    interpolated_points = []
+    for time in frame_time:
+        if float(time) >= min(time_curve) and float(time) <= max(time_curve):
+            interpolated_points.append(float(interpolate(float(time))))
+        else:
+            raise OutOfRangeError(f"Frame time {time} is out of range: min: {min(time_curve)} max: {max(time_curve)}")
+    
+    return interpolated_points
+
+    if plot:
+        # make a smooth curve from an expanded set of timepoints
+        more_time = numpy.linspace(min(time_curve), max(time_curve), num=1000, endpoint=True, retstep=False, dtype=None)
+        interpolated_curve = interpolate(more_time)
+
+        # print the original data against the interpolated curve
+        plt.plot(time_curve, whole_blood_radioactivity, 'o') 
+        plt.plot(more_time, interpolated_curve, '-')
+        plt.show()
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Returns a point along a curve fitted to the inputs from a blood tsv, used to' 
+                                    'match blood activity to frame timing of a pet scan.')
+
+    parser.add_argument('-m', '--manual-input', help='Manually sampled input file name', required=True)
+    parser.add_argument('-a', '--auto-input', help='Auto sampled input file name', required=False)
+    parser.add_argument('-t', '--frame-time', nargs='+', help='Frame time in seconds', required=True, type=float)
+    parser.add_argument('-p', '--plot', help='plot out a figure of a smoothly interpolated activity curve', required=False, action='store_true', default=False)
+
+
+    args = parser.parse_args()
+    
+    interpolated_points = interpolate(args.manual_input, args.auto_input, args.frame_time, args.plot)
+    for point in interpolated_points:
+        print(point)
